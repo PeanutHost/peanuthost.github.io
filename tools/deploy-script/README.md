10bb9e16bf16e3e14db1daaee9fc37bfcd203cdf
anonynorbi <git@anonynorbi.net>
2025-10-20 22:52:42 +0200

    Update deploy-script tool

--- tools/deploy-script/README.md
+++
@@ -1,7 +0,0 @@
-# PeanutHost's official website (public build)
-
-## License
-
-This project is licensed under the GNU General Public License v3.0 or later, **unless otherwise specified**.<br>
-This license applies **retroactively** to all published versions of this project.<br>
-See the [LICENSE](./LICENSE) file for the full license text, and [THIRD_PARTY_LICENSES](./THIRD_PARTY_LICENSES.md) for third-party components and their respective licenses.
--- tools/deploy-script/deploy.sh
+++ tools/deploy-script/deploy.sh
@@ -4,11 +4,11 @@ set -e
 DEST_DIR="../peanuthost-website-public/"
 
 echo "Cleaning and building Hugo site..."
-hugo --minify --cleanDestinationDir --gc -d "$DEST_DIR"
+hugo --minify --cleanDestinationDir --gc -d "${DEST_DIR}"
 
 echo "Copying readme and license files..."
-cp tools/deploy-script/README.md "$DEST_DIR"
-cp tools/deploy-script/LICENSE "$DEST_DIR"
-cp THIRD_PARTY_LICENSES.md "$DEST_DIR"
+cp ./README.md "${DEST_DIR}"
+cp ./LICENSE "${DEST_DIR}"
+cp ./THIRD_PARTY_LICENSES.md "${DEST_DIR}"
 
-echo "Build complete. Public site is ready in "$DEST_DIR""
+echo "Build complete. Public site is ready in '${DEST_DIR}'."
